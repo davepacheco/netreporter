1f90b42cbc2e4a13931845a58da0f43e630d2bf1
David Pacheco <dap@cs.brown.edu>
2019-03-20 21:00:38 -0700

    refactor in prep for supporting receive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+//
+// netreporter: primitive network monitoring tool
+//
+// This program currently just sends a single ICMP echo ("ping") packet to
+// 127.0.0.1.
+//
+
 extern crate pnet;
 
 use std::net::IpAddr;
@@ -9,19 +16,82 @@ use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
+use pnet::transport::TransportReceiver;
+use pnet::transport::TransportSender;
+
+// Name of the program itself
+const NR_ARG0 : &'static str = "netreporter";
 
 //
-// We make this large enough to support jumbograms.
+// Buffer size used for incoming ICMP packets.  We set this to 16K as a power of
+// two large enough to store typical jumbograms.  (We don't use these today, but
+// it may be useful in the future.)
 //
-const MAX_INCOMING_PACKET_SIZE : usize = 16384;
+const NR_MAX_INCOMING_PACKET_SIZE : usize = 16384;
 
 fn main()
 {
     let target : IpAddr = IpAddr::V4("127.0.0.1".parse().unwrap());
-    ping_send(target);
+
+    let mut icmp = match NrIcmpContext::new(NR_MAX_INCOMING_PACKET_SIZE) {
+        Ok(icmp) => icmp,
+        Err(err) => {
+            eprintln!("{}: {}", NR_ARG0, err);
+            std::process::exit(1);
+        }
+    };
+
+    match ping_send(&mut icmp, target) {
+        Ok(()) => println!("packet sent to {}", target),
+        Err(err) => {
+            eprintln!("{}: {}", NR_ARG0, err);
+            std::process::exit(1);
+        }
+    }
 }
 
-fn ping_send(target: IpAddr)
+//
+// NrIcmpContext encapsulates state related to sending and receiving ICMP
+// packets.  This includes channels on which to send and receive individual
+// packets, plus any other state (e.g., used for sequence numbers or the like).
+//
+struct NrIcmpContext {
+    #[allow(dead_code)]
+    nricmp_bufsize : usize,
+    nricmp_tx : TransportSender,
+    #[allow(dead_code)]
+    nricmp_rx : TransportReceiver,
+}
+
+impl NrIcmpContext {
+    pub fn new(bufsize : usize)
+        -> Result<NrIcmpContext, String>
+    {
+        let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
+
+        let (tx, rx) = match pnet::transport::transport_channel(
+            bufsize, protocol) {
+            Ok((tx, rx)) => (tx, rx),
+            Err(error) => {
+                return Err(format!(
+                    "failed to allocate ICMP transport channel: {}", error));
+            }
+        };
+
+        Ok(NrIcmpContext {
+            nricmp_bufsize: bufsize,
+            nricmp_tx : tx,
+            nricmp_rx : rx
+        })
+    }
+}
+
+//
+// Sends a single ICMP echo ("ping") packet to the target address.
+// XXX target address may as well be a V4 address.
+//
+fn ping_send(icmp : &mut NrIcmpContext, target: IpAddr)
+    -> Result<(), String>
 {
     let bufsz = MutableEchoRequestPacket::minimum_packet_size();
     let mut buffer : Vec<u8> = vec![0; bufsz];
@@ -49,18 +119,8 @@ fn ping_send(target: IpAddr)
     };
     packet.set_checksum(checksum);
 
-    let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
-    let mut tx = match pnet::transport::transport_channel(
-        MAX_INCOMING_PACKET_SIZE, protocol) {
-        Ok((tx, _)) => tx,
-        Err(error) => {
-            println!("failed to allocate ICMP transport channel: {}", error);
-            return;
-        }
-    };
-
-    match tx.send_to(packet, target) {
-        Ok(_) => println!("packet sent!"),
-        Err(error) => println!("failed to send ICMP packet: {}", error)
+    match icmp.nricmp_tx.send_to(packet, target) {
+        Ok(_) => Ok(()),
+        Err(error) => Err(format!("failed to send ICMP packet: {}", error))
     }
 }
